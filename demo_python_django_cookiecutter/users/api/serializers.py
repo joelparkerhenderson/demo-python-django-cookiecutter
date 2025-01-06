a14ce4679508235b954ed8b38f2099a01a897a67
Joel Parker Henderson <joel@joelparkerhenderson.com>
2025-01-05 17:00:39 -0800

    Init

---
+++ demo_python_django_cookiecutter/users/api/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+
+from demo_python_django_cookiecutter.users.models import User
+
+
+class UserSerializer(serializers.ModelSerializer[User]):
+    class Meta:
+        model = User
+        fields = ["name", "url"]
+
+        extra_kwargs = {
+            "url": {"view_name": "api:user-detail", "lookup_field": "pk"},
+        }
